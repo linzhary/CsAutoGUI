5b5c3e97fef04225a2cd025f588575759b939104
照秋 <luqihao@jzxf-group.cn>
2025-03-26 11:00:56 +0800

    优化代码

---
+++ CsAutoGUI.TestConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿using CsAutoGUI;
+
+var handle = AutoWindow.FindWindowLikeTitle("修仙聊天群MOD反馈群");
+var region = AutoWindow.GetWindowRegion(handle);
+AutoWindow.SetForegroundWindow(handle);
+foreach(var ch in "buzhidao")
+{
+    AutoKeyboard.Press(Enum.Parse<VirtualKey>(ch.ToString(), ignoreCase: true));
+}
+AutoKeyboard.Press(VirtualKey.SPACE);
+AutoKeyboard.Press(VirtualKey.RETURN);
